16f41befe6dd67f6465008aa6812268acdfa99ed
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2006-09-07 20:17:07 +0000

    WMS GroundOverlay

---
+++ trunk/tests/testwms.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+"""
+Copyright (C) 2006 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
+$URL$
+$Revision$
+$Date$
+"""
+
+import urllib
+
+import sys
+
+import kml.wms
+
+terra_url='http://terraservice.net/ogcmap.ashx?VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG:4326&WIDTH=256&HEIGHT=256&LAYERS=DRG,DRG,DRG&STYLES=GeoGrid_Red,GeoGrid_Red,GeoGrid_Red&TRANSPARENT=FALSE&FORMAT=image/jpeg&'
+
+region_w = kml.region.Region(40,39.9,-100,-100.1,'0')
+region_e = kml.region.Region(40,39.9,-99.9,-100,'0')
+
+wmskml = 'terra.kml'
+
+def EntityAmp(str):
+  n = []
+  ss = str.split('&')
+  for s in ss:
+    n.append(s)
+    n.append('&amp;')
+  return "".join(n)
+
+def GO(wmsurl,region,draworder,filename):
+  (n,s,e,w) = region.NSEW()
+  url = kml.wms.AppendWMSBBOX(wmsurl,n,s,e,w)
+  u = urllib.urlopen(url)
+  buf = u.read()
+  f = open(filename, 'w')
+  f.write(buf)
+  f.close()
+  u.close()
+
+  return kml.genkml.GroundOverlay(n,s,e,w,filename,0)
+  
+
+_kml = []
+_kml.append(kml.genkml.KML21())
+_kml.append('<Folder>\n')
+
+_kml.append('\n<Style>\n')
+_kml.append(kml.genkml.ListStyle('radioFolder'))
+_kml.append('</Style>\n')
+
+# Earth does WMS load
+
+_kml.append('\n<Folder><name>WMS</name>\n')
+_kml.append(kml.wms.WMSGroundOverlay(EntityAmp(terra_url), region_w, 0))
+_kml.append(kml.wms.WMSGroundOverlay(EntityAmp(terra_url), region_e, 0))
+_kml.append('</Folder>\n')
+
+# This script does WMS load to local file for Earth to show
+
+_kml.append('\n<Folder><name>local file</name>\n')
+_kml.append(GO(terra_url,region_w,0,'terra_w.jpg'))
+_kml.append(GO(terra_url,region_e,0,'terra_e.jpg'))
+_kml.append('</Folder>\n\n')
+
+_kml.append('</Folder>\n')
+_kml.append('</kml>\n')
+
+f = open(wmskml, 'w')
+f.write("".join(_kml))
+f.close()
+
