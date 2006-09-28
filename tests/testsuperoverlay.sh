dc92c01dd73ea699c09cd2424a7919a7e3f03143
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2006-09-28 20:04:01 +0000

    superoverlay unit tests

---
+++ tests/testsuperoverlay.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# superoverlay.py requires gdal and gdal python bindings...
+
+# http://landsat.gsfc.nasa.gov/data/Browse/Features/NASA_KSC.jpg
+
+ksc=NASA_KSC.jpg
+
+# will properly fail due to no bbox info in a .jpg
+superoverlay.py $ksc x x
+
+# use GroundOverlay LatLonBox
+superoverlay.py $ksc ksc-llb-0.kml llb-0.kml llb-0
+
+# use GroundOverlay LatLonBox + drawOrder
+superoverlay.py $ksc ksc-llb-d.kml llb-d.kml llb-d
+
+# use GroundOverlay LatLonBox + altitude
+superoverlay.py $ksc ksc-llb-a.kml llb-a.kml llb-a
+
+# use GroundOverlay LatLonBox + TimeSpan
+superoverlay.py $ksc ksc-llb-t.kml llb-t.kml llb-t
+
+# LatLonBox plus the full trifecta: drawOrder, altitude, TimeSpan
+superoverlay.py $ksc ksc-llb-3.kml llb-3.kml llb-3
