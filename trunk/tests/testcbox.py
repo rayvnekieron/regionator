a939b5b279f5e20418ce09f76644cf9c26c223de
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2006-10-30 01:37:00 +0000

    use unittest

--- trunk/tests/testcbox.py
+++ trunk/tests/testcbox.py
@@ -22,33 +22,51 @@ $Revision$
 $Date$
 """
 
-
+import unittest
 
 import kml.coordbox
 
-a = "2,2,0 4,4,0"
 
-c0 = kml.coordbox.CoordBox()
-c0.AddCoordinates(a)
-[lon,lat] = c0.MidPoint()
-if lon != 3 or lat != 3:
-	print 'ERROR: coordbox midpoint'
+class MidPointTestCase(unittest.TestCase):
+  def runTest(self):
+    a = "2,2,0 4,4,0"
+    c0 = kml.coordbox.CoordBox()
+    c0.AddCoordinates(a)
+    (lon,lat) = c0.MidPoint()
+    assert lon == 3, 'lon MidPoint wrong'
+    assert lat == 3, 'lat MidPoint wrong'
 
-c1 = kml.coordbox.CoordBox()
-n0 = 20
-s0 = 10
-e0 = 40
-w0 = 30
-c1.AddPoint(e0,n0)
-c1.AddPoint(w0,s0)
-c1.AddPoint(32,15)
-(n,s,e,w) = c1.NSEW()
-if n != n0 or s != s0 or e != e0 or w != w0:
-	print 'ERROR: coordbox latlonbox'
+class NSEWTestCase(unittest.TestCase):
+  def runTest(self):
+    c1 = kml.coordbox.CoordBox()
+    n0 = 20
+    s0 = 10
+    e0 = 40
+    w0 = 30
+    c1.AddPoint(e0,n0)
+    c1.AddPoint(w0,s0)
+    c1.AddPoint(32,15)
+    (n,s,e,w) = c1.NSEW()
+    assert n == n0, 'n NSEW wrong'
+    assert s == s0, 's NSEW wrong'
+    assert e == e0, 'e NSEW wrong'
+    assert w == w0, 'w NSEW wrong'
 
-c2 = kml.coordbox.CoordBox()
-c2.AddPoint(10,10)
-c2.AddPoint(20,20)
-size = c2.Size()
-print size
+class SizeTestCase(unittest.TestCase):
+  def runTest(self):
+    c2 = kml.coordbox.CoordBox()
+    c2.AddPoint(10,10)
+    c2.AddPoint(20,20)
+    size = c2.Size()
+    assert size == 10, 'size wrong'
+
+def suite():
+  suite = unittest.TestSuite()
+  suite.addTest(MidPointTestCase())
+  suite.addTest(NSEWTestCase())
+  suite.addTest(SizeTestCase())
+  return suite
+
+runner = unittest.TextTestRunner()
+runner.run(suite())
 
--- trunk/tests/testregion.py
+++ trunk/tests/testregion.py
@@ -22,6 +22,7 @@ $Revision$
 $Date$
 """
 
+import unittest
 
 
 # Test of some Region methods.
@@ -30,91 +31,72 @@ import kml.region
 import kml.genkml
 import kml.genxml
 
-print 'region Region start...'
 
-r = kml.region.Region(20,10,40,20,'0')
-print '0 ',r.NSEW()
-if r.Qid() != '0':
-  print 'ERROR: region Region Qid() failed',r.Qid()
-if r.Depth() != 1:
-  print 'ERROR: region Region Depth() failed',r.Depth()
-
-for q in ['0','1','2','3']:
-  rc = r.Child(q)
-  qid = '0' + q
-  if rc.Qid() != qid:
-    print 'ERROR: region Region qid() failed',rc.Qid()
-  if rc.Depth() != 2:
-    print 'ERROR: region Region Depth() failed',rc.Depth()
-
-for q in ['0','1','2','3']:
-  rc = r.Child(q)
-  print '0' + q,rc.NSEW()
-
-region = kml.region.Region(30,10,40,20,'0')
-
-region012 = region.Region('012')
-print region012.NSEW()
-
-region000 = region.Region('000')
-if region000.NSEW() != (30,25,25,20):
-  print 'ERROR: kml.region.Region()',region000.NSEW()
-
-region033 = region.Region('033')
-if region033.NSEW() != (15,10,40,35):
-  print 'ERROR: kml.region.Region()',region033.NSEW()
+class SimpleRegionTestCase(unittest.TestCase):
+  def setUp(self):
+    self.r = kml.region.Region(20,10,40,20,'0')
+  def testQid(self):
+    assert self.r.Qid() == '0', 'Qid bad'
+  def testDepth(self):
+    assert self.r.Depth() == 1, 'Depth bad'
+  def testChildren(self):
+    for q in ['0','1','2','3']:
+      rc = self.r.Child(q)
+      qid = '0' + q
+      assert rc.Qid() == qid, 'child Qid bad'
+      assert rc.Depth() == 2, 'child Depth bad'
 
 
-root = kml.region.RootRegion()
-n = 21.2
-s = 19.5
-e = -115.4
-w = -117.5
-r = root.Snap(n,s,e,w)
-(_n,_s,_e,_w) = r.NSEW()
-print 'orig',n,s,e,w
-print r.Qid(),_n,_s,_e,_w
+class SubRegionTestCase(unittest.TestCase):
+  def runTest(self):
+    region = kml.region.Region(30,10,40,20,'0')
+    region000 = region.Region('000')
+    region033 = region.Region('033')
+    assert region000.NSEW() == (30,25,25,20), 'subregion 000 bad'
+    assert region033.NSEW() == (15,10,40,35), 'subregion 033 bad'
 
-document = kml.genxml.Document()
-node = r.Qid()
-d = 1
-while d <= r.Depth():
-  qid = node[0:d]
-  _r = root.Region(qid)
-  (_n,_s,_e,_w) = _r.NSEW()
-  document.Add_Feature(kml.genkml.LatLonOutline(_n,_s,_e,_w,qid))
-  d += 1
 
-document.Add_Feature(kml.genkml.LatLonOutline(n,s,e,w,'orig'))
+def CreateAncestors(kmlfile):
+  root = kml.region.RootRegion()
+  n = 21.2
+  s = 19.5
+  e = -115.4
+  w = -117.5
+  r = root.Snap(n,s,e,w)
+  (_n,_s,_e,_w) = r.NSEW()
+  print 'orig',n,s,e,w
+  print r.Qid(),_n,_s,_e,_w
 
-k = kml.genxml.Kml()
-k.Feature = document.xml()
+  document = kml.genxml.Document()
+  node = r.Qid()
+  d = 1
+  while d <= r.Depth():
+    qid = node[0:d]
+    _r = root.Region(qid)
+    (_n,_s,_e,_w) = _r.NSEW()
+    document.Add_Feature(kml.genkml.LatLonOutline(_n,_s,_e,_w,qid))
+    d += 1
+  
+  document.Add_Feature(kml.genkml.LatLonOutline(n,s,e,w,'orig'))
 
-f = open('ancestors.kml','w')
-f.write(k.xml())
-f.close
+  k = kml.genxml.Kml()
+  k.Feature = document.xml()
+  
+  f = open(kmlfile,'w')
+  f.write(k.xml())
+  f.close
 
-r = kml.region.Region(20,10,50,40,'0')
 
-nw = kml.region.Location('0')
-print 'nw','0',nw
-if nw != (0,1):
-  print 'ERROR nw kml.region.Location()'
-
-ne = kml.region.Location('1')
-print 'ne','1',ne
-if ne != (1,1):
-  print 'ERROR ne kml.region.Location()'
-sw = kml.region.Location('2')
-
-print 'sw','2',sw
-if sw != (0,0):
-  print 'ERROR sw kml.region.Location()'
-se = kml.region.Location('3')
-
-print 'se','3',se
-if se != (1,0):
-  print 'ERROR se kml.region.Location()'
+class RegionLocationTestCase(unittest.TestCase):
+  def runTest(self):
+    nw = kml.region.Location('0')
+    ne = kml.region.Location('1')
+    sw = kml.region.Location('2')
+    se = kml.region.Location('3')
+    assert nw == (0,1), 'nw bad'
+    assert ne == (1,1), 'ne bad'
+    assert sw == (0,0), 'sw bad'
+    assert se == (1,0), 'se bad'
 
 n00 = kml.region.Grid('00')
 n01 = kml.region.Grid('01')
@@ -122,34 +104,47 @@ n02 = kml.region.Grid('02')
 n03 = kml.region.Grid('03')
 n000 = kml.region.Grid('000')
 
-n03 = kml.region.Grid('03')
-if n03 != (1,0):
-  print 'ERROR n03 kml.region.Grid()'
 
-n033 = kml.region.Grid('033')
-if n033 != (3,0):
-  print 'ERROR n033 kml.region.Grid()',n033
+class RegionGridTestCase(unittest.TestCase):
+  def runTest(self):
+    n03 = kml.region.Grid('03')
+    n033 = kml.region.Grid('033')
+    n0333 = kml.region.Grid('0333')
+    assert n03 == (1,0), '03 bad'
+    assert n033 == (3,0), '033 bad'
+    assert n0333 == (7,0), '0333 bad'
 
-n0333 = kml.region.Grid('0333')
-if n0333 != (7,0):
-  print 'ERROR n0333 kml.region.Grid()',n0333
 
-n0312312 = kml.region.Grid('0312312')
-n0333333 = kml.region.Grid('0333333')
-print '0312312',n0312312
-print '0333333',n0333333
-
-root = kml.region.RootRegion()
-kml_lon = -122.082163
-kml_lat =   37.420422
-max_depth = 18
-print 'SnapPoint',kml_lon,kml_lat
-ra = root.SnapPoint(kml_lon, kml_lat, max_depth)
-rb = kml.region.RootSnapPoint(kml_lon, kml_lat, max_depth)
-if ra.NSEW() != rb.NSEW():
-  print 'ERROR RootSnap failed'
-print ra.Depth(), ra.Qid()
-if ra.Depth() != max_depth:
-  print 'ERROR RootSnapPoint failed'
+class SnapPointTestCase(unittest.TestCase):
+  def runTest(self):
+    root = kml.region.RootRegion()
+    lon = -122.082163
+    lat =   37.420422
+    depth = 18
+    ra = root.SnapPoint(lon, lat, depth)
+    rb = kml.region.RootSnapPoint(lon, lat, depth)
+    (n,s,e,w) = ra.NSEW()
+    assert n == 37.42218017578125, 'north bad'
+    assert s == 37.41943359375, 'sourth bad'
+    assert e == -122.080078125, 'east bad'
+    assert w == -122.08282470703125, 'west bad'
+    assert ra.NSEW() == rb.NSEW(), 'bad NSEW'
+    assert ra.Depth() == depth, 'bad depth'
 
 print 'region Region ...end'
+
+def suite():
+  suite = unittest.TestSuite()
+  suite.addTest(SimpleRegionTestCase("testQid"))
+  suite.addTest(SimpleRegionTestCase("testDepth"))
+  suite.addTest(SimpleRegionTestCase("testChildren"))
+  suite.addTest(SubRegionTestCase())
+  suite.addTest(RegionLocationTestCase())
+  suite.addTest(RegionGridTestCase())
+  suite.addTest(SnapPointTestCase())
+  return suite
+
+runner = unittest.TextTestRunner()
+runner.run(suite())
+
+CreateAncestors('ancestors.kml')
