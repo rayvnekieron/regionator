32c40fcad7cc31bbdd5f32f6f4a930a56c367000
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2006-11-21 16:30:54 +0000

    convert to unittest

--- tests/testhref.py
+++ tests/testhref.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python
 
+import unittest
+
 import kml.region
 import kml.wms
 import kml.href
 
 
-def testhref(region, month):
-  wms_host = 'wms.jpl.nasa.gov'
-  wms_path = 'wms.cgi'
-  wms_queries = ['VERSION=1.1.1', 'REQUEST=GetMap', 'SRS=EPSG:4326',
+class SimpleTestCase(unittest.TestCase):
+  def runTest(self):
+    wms_host = 'wms.jpl.nasa.gov'
+    wms_path = 'wms.cgi'
+    wms_queries = ['VERSION=1.1.1', 'REQUEST=GetMap', 'SRS=EPSG:4326',
                 'WIDTH=256', 'HEIGHT=256', 'LAYERS=BMNG',
                 'TRANSPARENT=TRUE', 'FORMAT=image/png']
+    wms_month = 'Aug'
+    wms_north = 20
+    wms_south = 10
+    wms_east = -80
+    wms_west = -100
 
-  href = kml.href.Href()
-  href.SetHostname(wms_host)
-  href.SetPath(wms_path)
-  for wq in wms_queries:
-    href.AddQuery(wq)
-  href.AddQueryNameValue('STYLES', month)
-  href.AddQuery(kml.wms.WMSBBOX(region))
-  hrefstr = href.Href()
-  return hrefstr
+    want = 'http://wms.jpl.nasa.gov/wms.cgi?VERSION=1.1.1&amp;REQUEST=GetMap&amp;SRS=EPSG:4326&amp;WIDTH=256&amp;HEIGHT=256&amp;LAYERS=BMNG&amp;TRANSPARENT=TRUE&amp;FORMAT=image/png&amp;STYLES=Aug&amp;BBOX=-100.000000,10.000000,-80.000000,20.000000'
 
-region = kml.region.Region(20,10,-80,-100,'0')
+    href = kml.href.Href()
+    href.SetHostname(wms_host)
+    href.SetPath(wms_path)
+    for wq in wms_queries:
+      href.AddQuery(wq)
+    href.AddQueryNameValue('STYLES', wms_month)
+    region = kml.region.Region(wms_north, wms_south, wms_east, wms_west, '0')
+    href.AddQuery(kml.wms.WMSBBOX(region))
+    hrefstr = href.Href()
+    assert hrefstr == want
 
-got = testhref(region, 'Aug')
+def suite():
+  suite = unittest.TestSuite()
+  suite.addTest(SimpleTestCase())
+  return suite
 
-want = 'http://wms.jpl.nasa.gov/wms.cgi?VERSION=1.1.1&amp;REQUEST=GetMap&amp;SRS=EPSG:4326&amp;WIDTH=256&amp;HEIGHT=256&amp;LAYERS=BMNG&amp;TRANSPARENT=TRUE&amp;FORMAT=image/png&amp;STYLES=Aug&amp;BBOX=-100.000000,10.000000,-80.000000,20.000000'
+runner = unittest.TextTestRunner()
+runner.run(suite())
 
-if got != want:
-  print 'kml.href.Href FAILED'
-else:
-  print 'kml.href.Href okay'
