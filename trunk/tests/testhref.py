e0c3c86172039ba68af4e1e2a5c178ed4a5eb11d
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2006-12-13 01:33:19 +0000

    HttpTestCase(), FileTestCase() test SetUrl(), SetBasename(), GetScheme()

--- trunk/tests/testhref.py
+++ trunk/tests/testhref.py
@@ -9,6 +9,7 @@ import kml.href
 
 class SimpleTestCase(unittest.TestCase):
   def runTest(self):
+    wms_scheme = 'http'
     wms_host = 'wms.jpl.nasa.gov'
     wms_path = 'wms.cgi'
     wms_queries = ['VERSION=1.1.1', 'REQUEST=GetMap', 'SRS=EPSG:4326',
@@ -23,6 +24,7 @@ class SimpleTestCase(unittest.TestCase):
     want = 'http://wms.jpl.nasa.gov/wms.cgi?VERSION=1.1.1&amp;REQUEST=GetMap&amp;SRS=EPSG:4326&amp;WIDTH=256&amp;HEIGHT=256&amp;LAYERS=BMNG&amp;TRANSPARENT=TRUE&amp;FORMAT=image/png&amp;STYLES=Aug&amp;BBOX=-100.000000,10.000000,-80.000000,20.000000'
 
     href = kml.href.Href()
+    href.SetScheme(wms_scheme)
     href.SetHostname(wms_host)
     href.SetPath(wms_path)
     for wq in wms_queries:
@@ -31,11 +33,28 @@ class SimpleTestCase(unittest.TestCase):
     region = kml.region.Region(wms_north, wms_south, wms_east, wms_west, '0')
     href.AddQuery(kml.wms.WMSBBOX(region))
     hrefstr = href.Href()
-    assert hrefstr == want
+    assert hrefstr == want,'bad url [%s]' % hrefstr
+
+
+class HttpTestCase(unittest.TestCase):
+  def runTest(self):
+    href = kml.href.Href()
+    href.SetUrl('http://foo.com/dir/foo/hi.kml')
+    href.SetBasename('hello.kml')
+    assert href.Href() == 'http://foo.com/dir/foo/hello.kml'
+
+class FileTestCase(unittest.TestCase):
+  def runTest(self):
+    href = kml.href.Href()
+    href.SetUrl('/a/b/d/hi.kml')
+    assert href.GetScheme() == None,'scheme not none'
+
 
 def suite():
   suite = unittest.TestSuite()
   suite.addTest(SimpleTestCase())
+  suite.addTest(HttpTestCase())
+  suite.addTest(FileTestCase())
   return suite
 
 runner = unittest.TextTestRunner()
