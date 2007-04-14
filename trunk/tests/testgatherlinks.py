ce8940d3f49f5b9c8dbd3f77fb568f853e06d899
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2007-04-14 18:04:36 +0000

    NonExistentRootTestCase(), BasicAbsoluteTestCase()

--- trunk/tests/testgatherlinks.py
+++ trunk/tests/testgatherlinks.py
@@ -64,23 +64,39 @@ class BasicRbNLTestCase(unittest.TestCase):
       assert links.has_key(file), file
       filenum += 1
 
-class BasicHtmlTestCase(unittest.TestCase):
+class BasicRelativeTestCase(unittest.TestCase):
   def runTest(self):
-    links = kml.gatherlinks.GatherLinks(['-h','-r','-k'],'html.kml')
+    links = kml.gatherlinks.GatherLinks(['-h','-k','-r'],'html.kml')
     assert 1 == links['../sketchup_building_golden1.png']
     assert 1 == links['../3dwh-logo_en.gif']
     assert 1 == links['../../placemarks/99/1952f8d3e8d27fe065a153e1a3d3359c.jpg']
     assert 3 == len(links)
 
+class BasicAbsoluteTestCase(unittest.TestCase):
+  def runTest(self):
+    links = kml.gatherlinks.GatherLinks(['-h','-k','-a'],'html.kml')
+    assert 5 == len(links)
+    assert 1 == links['http://sketchup.google.com/support/bin/answer.py?answer=57057&hl=en']
+    assert 1 == links['http://sketchup.google.com/support/bin/request.py?contact_type=reportmodel&hl=en&mid=1952f8d3e8d27fe065a153e1a3d3359c']
+    assert 1 == links['http://sketchup.google.com/3dwarehouse/search?uq=00986856740277826572&hl=en']
+    assert 1 == links['http://sketchup.google.com/3dwarehouse?hl=en']
+    assert 1 == links['http://sketchup.google.com/3dwarehouse/details?mid=1952f8d3e8d27fe065a153e1a3d3359c&hl=en']
+
+class NonExistentRootTestCase(unittest.TestCase):
+  def runTest(self):
+    status = kml.gatherlinks.GatherLinks('','no-such-file-or-url')
+    print status
+    assert None == kml.gatherlinks.GatherLinks('','no-such-file-or-url')
 
 def suite(): 
   suite = unittest.TestSuite()
   suite.addTest(BasicRbNLTestCase("testGatherNoLinks"))
   suite.addTest(BasicRbNLTestCase("testGatherRelativeKmlLinks"))
-  suite.addTest(BasicHtmlTestCase())
+  suite.addTest(BasicRelativeTestCase())
+  suite.addTest(BasicAbsoluteTestCase())
+  suite.addTest(NonExistentRootTestCase())
   return suite
 
 runner = unittest.TextTestRunner()
 runner.run(suite())
-
 
