bea3e26ae2b953bb1cf1d5a720440be4dcd7ee81
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2007-05-03 04:30:39 +0000

    -e encoding overrides xml encoding

--- scripts/checklinks.py
+++ scripts/checklinks.py
@@ -6,14 +6,16 @@ import sys
 import kml.checklinks
 
 if len(sys.argv) < 2:
-  print 'usage: %s [-k] [-h] [-a] [-r] [-v] [-s] [-c] url.kml' % sys.argv[0]
+  print 'usage: %s [-k] [-h] [-a] [-r] [-v] [-s] [-c] [-e encoding] url.kml' \
+                                                                  % sys.argv[0]
   print '   -k: check KML hrefs'
   print '   -h: check HTML hrefs'
-  print '   -r: check relative URLs'
   print '   -a: check absolute URLs'
+  print '   -a: check relative URLs'
   print '   -c: compute checksum'
   print '   -v: verbose'
   print '   -s: print summary only'
+  print '   -e encoding: override xml encoding'
   sys.exit(1)
 
 inputkml = sys.argv[len(sys.argv)-1]
