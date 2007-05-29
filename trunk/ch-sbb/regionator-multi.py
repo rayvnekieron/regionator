6b0be963015e3b3dc797066e36280ed4a38c4ede
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2007-05-29 03:18:07 +0000

    factor out common functions

--- trunk/ch-sbb/regionator-multi.py
+++ trunk/ch-sbb/regionator-multi.py
@@ -9,6 +9,7 @@ import kml.dashboard
 import kml.featureset
 import kml.genkml
 import kml.genxml
+import chsbb
 
 station_point_csv='ch-stations.csv'
 station_connections_csv='ch-connections.csv'
@@ -20,66 +21,11 @@ dir='ch-multi-dir'
 minlod = 256
 maxper = 16
 
-def CreateConnectionLines(lon, lat, connection_list):
-  """
-  Args:
-    connection_list: list of (lon, lat)
-  Returns:
-    linestring_kml_list: list of '<LineString>...</LineString>'
-  """
-  linestring_kml_list = []
-  for (conn_lon, conn_lat) in connection_list:
-    linestring_kml = kml.genkml.SimpleLineString(lon, lat, conn_lon, conn_lat)
-    linestring_kml_list.append(linestring_kml)
-  return linestring_kml_list
-
-def CreatePlacemark(lon, lat, name, description, connection_list, style_url):
-  """Create a MultiGeometry Placemark
-  Args:
-    lon, lat: location
-    name: station name (unicode)
-    connection_list: list of tuples: (station_name, connection_count)
-    style_url: url#id to kml file with Style of id=id
-  Returns:
-    kml: '<Placemark>...<MultiGeometry>..</MultiGeometry</Placemark>'
-  """
-  multigeometry = kml.genxml.MultiGeometry()
-  multigeometry.AddGeometry(kml.genkml.Point(lon,lat))
-  for linestring_kml in CreateConnectionLines(lon, lat, connection_list):
-    multigeometry.AddGeometry(linestring_kml)
-  placemark = kml.genxml.Placemark()
-  placemark.name = name
-  placemark.description = description
-  placemark.styleUrl = style_url
-  placemark.Set_Geometry(multigeometry.xml())
-  return placemark.xml()
-  
-
 # Parse the stations CSV to create a map of name to station info.
-stations = {}
-f = open(station_point_csv, 'r')
-for csv_line in f:
-  (score, lon, lat, name, description, style_url) = \
-        kml.csvregionator.ParseCsvLine(csv_line, 'utf-8')
-  stations[name] = (score, lon, lat, name, description)
+stations = chsbb.ParseStationInfoCsv(station_point_csv)
 
 # Parse the connections CSV to create a map of name to a list of connections.
-connections = {}
-def AddConnection(from_station, to_station):
-  if connections.has_key(from_station):
-    connections[from_station].append(to_station)
-  else:
-    connections[from_station] = [to_station]
-
-f = open(station_connections_csv, 'r')
-for csv_line in f:
-  tuple = csv_line.split('|')
-  if len(tuple) > 2:
-    from_station = tuple[0].decode('utf-8')
-    to_station = tuple[1].decode('utf-8')
-    AddConnection(from_station, to_station)
-    AddConnection(to_station, from_station)
-
+connections = chsbb.ParseConnections(station_connections_csv)
 
 # Get the list of (lon,lat) tuples of connections from this station.
 def GetLocations(station_name):
@@ -100,8 +46,8 @@ feature_set = kml.featureset.FeatureSet()
 for name in stations.keys():  # XXX proper iterator
   (score, lon, lat, name, description) = stations[name]
   connection_list = GetLocations(name)
-  placemark_kml = CreatePlacemark(lon, lat, name, description,
-                                  connection_list, styleurl)
+  placemark_kml = chsbb.CreateMultiPlacemark(lon, lat, name, description,
+                                             connection_list, styleurl)
   feature_set.AddWeightedFeatureAtLocation(score, lon, lat, placemark_kml)
 
 # Sort the FeatureSet and regionate.
