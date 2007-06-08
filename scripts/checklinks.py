773d62fbd720c9cfeee6bb8706eb2a239231c5ea
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2007-06-08 15:12:25 +0000

    r is for relative

--- scripts/checklinks.py
+++ scripts/checklinks.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 
+"""
+Copyright (C) 2007 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
+$URL$
+$Revision$
+$Date$
+"""
+
+
 # Check all link targets in a KML file/hierarchy.
 
 import sys
@@ -11,7 +34,7 @@ if len(sys.argv) < 2:
   print '   -k: check KML hrefs'
   print '   -h: check HTML hrefs'
   print '   -a: check absolute URLs'
-  print '   -a: check relative URLs'
+  print '   -r: check relative URLs'
   print '   -c: compute checksum'
   print '   -v: verbose'
   print '   -s: print summary only'
