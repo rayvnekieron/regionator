b1c81a4d54d0617a71320513a4b0840ee0a470c6
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2007-06-27 01:33:25 +0000

    document -u tar

--- scripts/checklinks.py
+++ scripts/checklinks.py
@@ -44,6 +44,7 @@ if len(sys.argv) < 2:
   print '   -e encoding: override xml encoding'
   print '   -u url.kml: KML/KMZ file or hierarchy to check'
   print '   -u dir: root of directory hierarchy of KML/KMZ files'
+  print '   -u archive.tgz: a tar archive of KML/KMZ files'
   sys.exit(1)
 
 status = kml.checklinks.CheckLinks(sys.argv[1:])
