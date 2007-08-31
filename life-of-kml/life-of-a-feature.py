e2b882bd8ef7f6b119d118f44163635171b032c5
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2007-08-31 19:23:59 +0000

    add LookAt, ExtendedData

--- life-of-kml/life-of-a-feature.py
+++ life-of-kml/life-of-a-feature.py
@@ -5,6 +5,7 @@
 # A demonstration of KML elements and behavior common to all Features
 
 import kml.genxml
+import kml.genkml
 import sys
 
 loafkml = 'life-of-a-feature.kml'
@@ -15,6 +16,7 @@ def CreateStyle(id):
   text = []
   text.append('<b>$[name]</b>')
   text.append('$[description]')
+  text.append('Longitude=$[lon], Latitude=$[lat]')
   text.append('<i>$[id]</i>')
   balloonstyle.text = '<![CDATA[%s]]>' % '<br/>'.join(text)
   balloonstyle.bgColor = 'ff82fff3' # light yellow
@@ -39,11 +41,17 @@ def CreateExtendedData(nvp_list):
   for (name,value) in nvp_list:
     data = kml.genxml.Data()
     data.name = name
-    data.value = id
+    data.value = value
     extendeddata.Add_Data(data.xml())
   return extendeddata
 
-def SetFeatureStuff(feature, short, long, id, style_url, nvp_list):
+def CreateLonLat(lon, lat):
+  ll = []
+  ll.append(('lon', lon))
+  ll.append(('lat', lat))
+  return ll
+
+def SetFeatureFields(id, feature, short, long, view, style_url, nvp_list):
   # Object-ness
   feature.id = id
 
@@ -58,39 +66,80 @@ def SetFeatureStuff(feature, short, long, id, style_url, nvp_list):
   # feature.phoneNumber
   feature.Snippet = long
   feature.description = long
-  # feature.AbstractView
+  feature.AbstractView = view
   # feature.TimePrimitive
   feature.styleUrl = style_url
   # feature.StyleSelector
   # feature.Region
-  # feature.ExtendedData = CreateExtendedData(nvp_list)
+  if nvp_list:
+    feature.Add_ExtendedData(CreateExtendedData(nvp_list).xml())
 
 def CreateLOAF():
   style_id = 'shared-style'
-  style_url = '#%s' % style_id
+  styurl = '#%s' % style_id
 
   style = CreateStyle(style_id)
 
   placemark = kml.genxml.Placemark()
-  SetFeatureStuff(placemark, 'PM', 'Placemark', 'pm0', style_url, None)
+  lon = 134.6
+  lat = -20.47
+  view = kml.genkml.LookAt(lon, lat, 3473786, 17.8, 179)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] includes the Austrialian mainland, ' \
+         'New Guinea, and Tasmania.'
+  SetFeatureFields('pm0', placemark, 'Australia', desc, view, styurl, nvp)
 
   networklink = kml.genxml.NetworkLink()
-  SetFeatureStuff(networklink, 'NL', 'NetworkLink', 'nl0', style_url, None)
+  lon = 138.3
+  lat = -86.37
+  view = kml.genkml.LookAt(lon, lat, 4715247, 0, -140.8)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] is the Earth\'s southernmost continent and covers ' \
+         'the South Pole.'
+  SetFeatureFields('nl0', networklink, 'Antarctica', desc, view, styurl, nvp)
 
   folder = kml.genxml.Folder()
-  SetFeatureStuff(folder, 'F', 'Folder', 'f0', style_url, None)
+  lon = 18.37
+  lat = 49.18
+  view = kml.genkml.LookAt(lon, lat, 3430622, 19.8, -51.72)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] is south of the Arctic, north of the Mediterranean ' \
+         'east of the Atlantic and west of the Caucasas Mountains'
+  SetFeatureFields('f0', folder, 'Europe', desc, view, styurl, nvp)
 
   document = kml.genxml.Document()
-  SetFeatureStuff(document, 'D', 'Document', 'd0', style_url, None)
+  lon = 13.15
+  lat = -1.01
+  view = kml.genkml.LookAt(lon, lat, 5641142, 10.9, 27.13)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] is the world\'s second largest continent and is ' \
+         'the second most populous continent'
+  SetFeatureFields('d0', document, 'Africa', desc, view, styurl, nvp)
 
   groundoverlay = kml.genxml.GroundOverlay()
-  SetFeatureStuff(groundoverlay, 'GO', 'GroundOverlay', 'go0', style_url, None)
+  lon = -67.09
+  lat = -22.48
+  view = kml.genkml.LookAt(lon, lat, 5641142, 15.1, 41.21)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] is east of the Pacific and west of the Atlantic'
+  SetFeatureFields('go0', groundoverlay, 'South America', desc, view, styurl, nvp)
 
   screenoverlay = kml.genxml.ScreenOverlay()
-  SetFeatureStuff(screenoverlay, 'SO', 'ScreenOverlay', 'so0', style_url, None)
+  lon = 102
+  lat = 33.7
+  view = kml.genkml.LookAt(lon, lat, 4715247, 0, 9.87)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] is the world\'s largest continent in both land ' \
+         'area and population.'
+  SetFeatureFields('so0', screenoverlay, 'Asia', desc, view, styurl, nvp)
 
   photooverlay = kml.genxml.PhotoOverlay()
-  SetFeatureStuff(photooverlay, 'PO', 'PhotoOverlay', 'po0', style_url, None)
+  lon = -100.7
+  lat = 31.7
+  # view = kml.genkml.LookAt(lon, lat, 4193430, 15.44, 4.72)
+  nvp = CreateLonLat(lon, lat)
+  desc = '$[name] is south of the Artic and north of the Carribean'
+  SetFeatureFields('po0', photooverlay, 'North America', desc, None, styurl, nvp)
 
   d = kml.genxml.Document()
   d.name = 'Life of a Feature'
