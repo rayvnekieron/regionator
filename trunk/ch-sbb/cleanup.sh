89fc79e535e0df359d75b59061717f2e5f5cea16
regionator <regionator@0c7ec548-a719-0410-a32c-a55d6bba7b11>
2008-11-21 17:15:02 +0000

    removing

--- trunk/ch-sbb/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf ch-basic-root.kml ch-basic-dir 
-rm -f style/style.kml
-rm -rf ch-style-root.kml ch-style-dir 
-rm -rf ch-multi-root.kml ch-multi-dir 
-rm -f db.kml
-
--- trunk/ch-sbb/regionate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-./cleanup.sh
-./regionator-basic.sh
-ln -s balloon.kml style/style.kml
-./regionator-style.sh
-./regionator-multi.py
